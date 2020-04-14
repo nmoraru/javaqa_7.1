13f81faa4300e20846db562549d94c9c7d680e43
nikita moraru <test@mail.com>
2020-04-14 21:54:00 +0300

    added test firstElemNotMax

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,11 +7,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class StatisticsServiceTest {
 
   @Test
-  void findMax() {
+  void firstElemMax() {
     StatisticsService service = new StatisticsService();
 
     long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
     long expected = 12;
+
+    long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void firstElemNotMax() {
+    StatisticsService service = new StatisticsService();
+
+    long[] incomesInBillions = {12, 13, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+    long expected = 13;
 
     long actual = service.findMax(incomesInBillions);
 
