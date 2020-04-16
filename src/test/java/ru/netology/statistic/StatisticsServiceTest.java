1edf4a2335ea990c97d3ff47154e08b0b5d009ec
nikita moraru <test@mail.com>
2020-04-16 16:46:51 +0300

    rename test firstElemNotMax --> firstElemUnderMax

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -19,7 +19,7 @@ class StatisticsServiceTest {
   }
 
   @Test
-  void firstElemNotMax() {
+  void firstElemUnderMax() {
     StatisticsService service = new StatisticsService();
 
     long[] incomesInBillions = {12, 13, 8, 4, 5, 3, 8, 6, 11, 11, 12};
